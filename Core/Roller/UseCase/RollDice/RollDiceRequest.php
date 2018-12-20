5e2301c0cc514bdfef0b36a41e9f8e147bd9137f
Mateusz Procner <mateusz.procner@lizardmedia.pl>
2018-12-20 15:40:00 +0100

    use case refactor

---
+++ Core/Roller/UseCase/RollDice/RollDiceRequest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * File: RollDiceRequest.php
+ *
+ * @author Mateusz Procner<mateusz.procner@lizardmedia.pl>
+ * @copyright Copyright (C) 2018 Lizard Media (http://lizardmedia.pl)
+ */
+
+namespace Core\Roller\UseCase\RollDice;
+
+class RollDiceRequest
+{
+
+}
---
+++ Core/Roller/UseCase/RollDice/RollDiceResponse.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * File: RollDiceResponse.php
+ *
+ * @author Mateusz Procner<mateusz.procner@lizardmedia.pl>
+ * @copyright Copyright (C) 2018 Lizard Media (http://lizardmedia.pl)
+ */
+
+namespace Core\Roller\UseCase\RollDice;
+
+class RollDiceResponse
+{
+
+}
